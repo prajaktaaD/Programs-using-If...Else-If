2b12ec1f77092e304e2dcbb43ce6e87e70870c18
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-05 09:09:53 +0530

    Create If_ElseIf1.c
    Find the greatest of three numbers using if-else if

---
+++ If_ElseIf1.c
@@ -0,0 +1,31 @@
+//1. Find the greateat of three numbers:
+
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("Enter three distinct integers:");
+  scanf("%d%d%d",&a,&b,&c);
+  
+  if(a>b)
+    {
+       if(a>c)
+         printf("The greatest is a(%d)",a);
+        
+       else
+         printf("The greatest is c(%d)",c);
+     }
+  else
+  if(a<b)
+   {
+       if(b>c)
+         printf("The greatest is b(%d)",b);
+         
+       else
+        printf("The greatest is c(%d)",c)
+  }
+  
+  return 0;
+  
+}  
+  
